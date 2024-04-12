c4f61c3f1f2cb80cd224a990e4a8fc198e752a3b
shin0624 <91828379+shin0624@users.noreply.github.com>
2024-04-12 12:43:06 +0900

    플레이어 회전 및 벽뚫기 오류 수정
    1. 플레이어가 벽을 뚫고 들어가는 오류 수정 : 이전 코드에서, 플레이어 이동을 transform.position으로 구현하였기에 키 입력 시 플레이어 위치가 강제로 변화하면서 벽뚫기 발생.-->RigidBody의 MovePosition()(보간세팅)을 사용하여 매 프레임마다 자연스럽게 움직이도록 함.
    2. 플레이어 y축 회전 시 카메라 떨림 수정 : 입력 스무딩을 적용하여 입력 값을 이전 프레임의 값과 적절히 조합함. 부드러운 회전 가능.
    3. 카메라가 벽을 통과하는 오류 수정 : MainCamera의 위치를 Player 오브젝트 내에 물리적 삽입

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,32 +4,76 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
+    Rigidbody rb;
+
+    [Header("Rotate")]
+    public float mouseSpeed;//개발 단계 Inspector창에서의 빠른 수정를 위해 public으로 선언하나, 코드 안정화 후 캡슐화를 위해 Serialized Field로 설정할 것.
+    float yRotation;
+    float xRotation;
+    Camera cam;//CameraController를 따로 작성하지 않고, GameObject를 상속받는 Camera타입을 가져와 선언.
+
+    float smoothXRotation;
+    float smoothYRotation;//카메라 회전 시 떨림 방지를 위한 입력 스무딩 적용.
+    public float rotationSmoothTIme = 0.1f;//입력 스무딩에 사용될 시간. Inspector에서 조정가능.
+
+    [Header("Move")]
+    public float moveSpeed;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;   // 마우스 커서를 화면 안에서 고정
+        Cursor.visible = false;                     // 마우스 커서를 보이지 않도록 설정
+
+        rb = GetComponent<Rigidbody>();             // Rigidbody 컴포넌트 가져오기
+        rb.freezeRotation = true;                   // Rigidbody의 회전을 고정하여 물리 연산에 영향을 주지 않도록 설정
+
+        cam = Camera.main;                          // 메인 카메라를 할당
+        
+    }
 
     void Update()
     {
-        OnKeyboard();
+        Rotate();//매 프레임마다 회전 업데이트
     }
 
-    void OnKeyboard()
+    void FixedUpdate()//Move()를 따로 처리하기 위한 FIxedUpdate()
     {
-        Vector3 moveDirection = Vector3.zero;
+        Move();//매 프레임마다 이동 업데이트
+    }
 
-        if (Input.GetKey(KeyCode.W))
-            moveDirection += Vector3.forward;
-        if (Input.GetKey(KeyCode.S))
-            moveDirection += Vector3.back;
-        if (Input.GetKey(KeyCode.A))
-            moveDirection += Vector3.left;
-        if (Input.GetKey(KeyCode.D))
-            moveDirection += Vector3.right;
 
-        // 플레이어를 이동시키고 회전값을 카메라의 회전값으로 설정
-        if (moveDirection != Vector3.zero)
-        {
-            transform.position += moveDirection * Time.deltaTime * _speed;
-            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
-        }
+    void Move()
+    {
+        //h,v를 지역변수로 설정하여 move호출 시에만 메모리를 할당하므로 메모리 사용 최적화 가능
+       float h = Input.GetAxisRaw("Horizontal"); // 수평 이동 입력 값
+       float v = Input.GetAxisRaw("Vertical");   // 수직 이동 입력 값
+
+        // 입력에 따라 이동 방향 벡터 계산
+        Vector3 moveVec = transform.forward * v + transform.right * h;
+
+        //Rigidbody를 이용하여 이동하게 하여 벽 통과를 방지.
+        rb.MovePosition(rb.position + moveVec.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    void Rotate()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSpeed * Time.deltaTime;
+#if 카메라떨림보정_수정전
+        yRotation += mouseX;    // 마우스 X축 입력에 따라 수평 회전 값을 조정
+        xRotation -= mouseY;    // 마우스 Y축 입력에 따라 수직 회전 값을 조정
+#endif
+        //입력스무딩을 이용하여 카메라 떨림을 보정. 0.1이 가장 적절.
+        smoothXRotation = Mathf.SmoothDamp(smoothXRotation, mouseX, ref smoothXRotation, rotationSmoothTIme);
+        smoothYRotation = Mathf.SmoothDamp(smoothYRotation, mouseY, ref smoothYRotation, rotationSmoothTIme);
+
+        yRotation += smoothXRotation;
+        xRotation -= smoothYRotation;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // 수직 회전 값을 -90도에서 90도 사이로 제한
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0); // 카메라의 회전을 조절
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);             // 플레이어 캐릭터의 회전을 조절
     }
 }
