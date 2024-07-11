5ea6c47ff15a2fbf318185c386567fd0c7937fdf
shin0624 <91828379+shin0624@users.noreply.github.com>
2024-07-11 18:11:03 +0900

    Singleton 적용

--- Auto_Scripts/AutoShutterScript.cs
+++ Auto_Scripts/AutoShutterScript.cs
@@ -8,16 +8,32 @@ using UnityEngine;
 
 public class AutoShutterScript : MonoBehaviour
 { 
+    public static AutoShutterScript Instance { get; private set; }//오토셔터 인스턴스를 전역으로 선언-> 씬 전환 후에도 셔터의 RaiseShutter값이 변하지 않아야 하기 때문
     public bool RaiseShutter;//셔터가 올라갔는지 유무
+    
     private OpenShieldController _sh;//보안실 배전함 스크립트에서 레버가 올라갔는지 유무를 확인하여 참조
+  
     private Vector3 RaisPosition;//셔터 올라가는 위치벡터
     public float RaiseLerpTime = 0.1f;//보간시간
     public AudioSource Ado;//셔터 효과음
 
 
+    public void Awake()//초기화
+    {
+        if(Instance==null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-        RaiseShutter = false;
+       // RaiseShutter = false;
         RaisPosition = transform.position + new Vector3(0, 2.9f, 0);//y축으로 2.96 +되어야 딱 맞음
 
         _sh = GameObject.FindFirstObjectByType<OpenShieldController>();
@@ -38,12 +54,16 @@ public class AutoShutterScript : MonoBehaviour
     
     void Update()
     {
-        if (_sh.leverRaised == true)//배전함 레버가 올라가면
+        if (_sh ==null)
+        {
+            _sh = FindOpenShieldController();
+        }
+
+        if(_sh != null && _sh.leverRaised)
         {
             Raise();
         }
-
-        if(RaiseShutter && Vector3.Distance(transform.position, RaisPosition) <0.01f)
+        if(RaiseShutter && Vector3.Distance(transform.position, RaisPosition) < 0.01f)
         {
             _sh.ResetLever(); //셔터가 목표 위치까지 올라왔다면 leverRaised값을 false로 변경. 이렇게 해야 사운드 반복재생이 안된다.
         }
@@ -61,6 +81,10 @@ public class AutoShutterScript : MonoBehaviour
                     Ado.Play();
                     Ado.loop = false;
                 }
+    }
 
+    private OpenShieldController FindOpenShieldController()
+    {
+        return GameObject.FindFirstObjectByType<OpenShieldController>();
     }
 }
