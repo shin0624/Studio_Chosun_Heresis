fce972cd12eaa31f02293d69b4025a0c7908bde0
rkdsan1013 <164186876+rkdsan1013@users.noreply.github.com>
2024-07-21 20:40:12 +0900

    Add files via upload

--- DialogueManager.cs
+++ DialogueManager.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 using System.Collections.Generic;
 
 public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText;
-
     private Dictionary<string, string> dialogueDictionary = new Dictionary<string, string>();
 
     void Start()
@@ -28,8 +28,50 @@ public class DialogueManager : MonoBehaviour
     {
         if (dialogueDictionary.ContainsKey(dialogueID))
         {
+            StopAllCoroutines();
             dialogueText.text = dialogueDictionary[dialogueID];
+            StartCoroutine(FadeInDialogue(1f));
+            StartCoroutine(ClearDialogueAfterDelay(5f));
         }
+    }
+
+    private IEnumerator ClearDialogueAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        StartCoroutine(FadeOutDialogue(1f));
+    }
+
+    private IEnumerator FadeInDialogue(float duration)
+    {
+        Color color = dialogueText.color;
+        color.a = 0;
+        dialogueText.color = color;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / duration);
+            dialogueText.color = color;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOutDialogue(float duration)
+    {
+        Color color = dialogueText.color;
+        float startAlpha = color.a;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(startAlpha - (elapsedTime / duration));
+            dialogueText.color = color;
+            yield return null;
+        }
+
+        dialogueText.text = string.Empty;
     }
 }
 
