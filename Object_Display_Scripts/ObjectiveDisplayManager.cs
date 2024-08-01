06cae8cc7cfda39a2811482254cdafbbe934ead6
rkdsan1013 <164186876+rkdsan1013@users.noreply.github.com>
2024-08-02 04:21:01 +0900

    Add files via upload

--- Object_Display_Scripts/ObjectiveDisplayManager.cs
+++ Object_Display_Scripts/ObjectiveDisplayManager.cs
@@ -7,7 +7,6 @@ public class ObjectiveDisplayManager : ObjectiveController
 {
     //게임 상에서 발생하는 모든 목표(Objective)출력을 제어할 스크립트
     //DialogueDisplayManger와 동일한 구조로 작성함.
-
     private Dictionary<string, bool> ObjectDisplay = new Dictionary<string, bool>();//목표가 중복 출력되는 것을 방지하기 위해 목표번호(숫자 + a)와 출력 여부를 딕셔너리로 관리
 
     protected override void OnStart()
@@ -17,7 +16,6 @@ public class ObjectiveDisplayManager : ObjectiveController
 
     protected override void OnUpdate()
     {
-        
     }
 
     private void OnDestroy()
@@ -27,8 +25,6 @@ public class ObjectiveDisplayManager : ObjectiveController
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)//씬이 전환될 때 마다 적절한 목표가 출력될 수 있도록 하는 함수
     {
-
-
     }
 
     public void DisplayObjectiveOnce(string newObjectiveID, string objectiveID)//메인 목표가 한번만 출력될 수 있도록 하는 메서드.추상클래스의 DisplayObjective를 사용하여 목표를 출력하고, 그때그때 출력된 목표 TF값을 VALUE로 한다
@@ -41,7 +37,6 @@ public class ObjectiveDisplayManager : ObjectiveController
         }
     }
 
-
     private void OnCollisionEnter(Collision collision)//특정 장소, 특정 아이템 등 충돌 시 나타나는 다이얼로그 표시
     {
 
@@ -49,17 +44,70 @@ public class ObjectiveDisplayManager : ObjectiveController
         {
             if (collision.gameObject.CompareTag("1F_Start"))
             {
-
-                FindSecurityRoom();
-
+                ExploreFirstFloor()//병원 탐색하기
+            }
+            else if (collision.gameObject.CompareTag(""))
+            {
+                FindToolForCrackedWall()//금간 벽을 부술 수 있는 도구를 찾기
+            }
+            else if (collision.gameObject.CompareTag(""))
+            {
+                FindSecurityRoom();//단서에 적힌 보안실 비밀번호 알아내기
+            }
+            else if (collision.gameObject.CompareTag("2F_Start"))
+            {
+                ExploreSecondFloor();//2층 탐색하기
+            }
+            else if (collision.gameObject.CompareTag(""))
+            {
+                FindPathToLightRoom()//불 켜진 방으로 향하는 길 찾기
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "")//지하 씬에서 출력될 다이얼로그
+        {
+            if (collision.gameObject.CompareTag(""))
+            {
+                ExploreBasement()//지하 탐색하기
+            }
+            else if (collision.gameObject.CompareTag(""))
+            {
+                Escape();//탈출할 방법 찾기
             }
         }
     }
 
-
-    void FindSecurityRoom()//단서에 적힌 보안실 비밀번호 알아내기
+    void ExploreFirstFloor()//병원 탐색하기
     {
         DisplayObjectiveOnce("001a", "001b");
     }
 
+    void FindToolForCrackedWall()//금간 벽을 부술 수 있는 도구를 찾기
+    {
+        DisplayObjectiveOnce("002a", "002b");
+    }
+
+    void FindSecurityRoom()//단서에 적힌 보안실 비밀번호 알아내기
+    {
+        DisplayObjectiveOnce("003a", "003b");
+    }
+
+    void ExploreSecondFloor()//2층 탐색하기
+    {
+        DisplayObjectiveOnce("004a", "004b");
+    }
+
+    void FindPathToLightRoom()//불 켜진 방으로 향하는 길 찾기
+    {
+        DisplayObjectiveOnce("005a", "005b");
+    }
+
+    void ExploreBasement()//지하 탐색하기
+    {
+        DisplayObjectiveOnce("101a", "101b");
+    }
+
+    void Escape()//탈출할 방법 찾기
+    {
+        DisplayObjectiveOnce("999a", "999b");
+    }
 }
