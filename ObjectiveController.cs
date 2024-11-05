63120b3a84ace3afa0933a7b95bc11fc7011dbb5
SHIN BEOM SU <91828379+shin0624@users.noreply.github.com>
2024-11-05 23:45:15 +0900

    도중 제작/수정한 스크립트 정리

--- ObjectiveController.cs
+++ ObjectiveController.cs
@@ -21,5 +21,8 @@ public abstract class ObjectiveController : MonoBehaviour
     protected void DisplayObjective(string newObjectiveID, string objectiveID)
     {
         objectiveManager.DisplayObjectives(newObjectiveID, objectiveID);
+        //newObjectiveID : 가운데에 출력된 후 페이드아웃되는 목표
+        //objectiveID : 오른쪽에 계속 출력되는 목표
+        //JSON 파일에서 : 숫자 + a가ㅣ newObjectiveID, 숫자 + B가 objectiveID
     }
 }
