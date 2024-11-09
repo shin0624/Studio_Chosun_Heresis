a97c4f18a75f56f80936aa05423ce191cc01b854
SHIN BEOM SU <91828379+shin0624@users.noreply.github.com>
2024-11-09 11:45:24 +0900

    SceneChangeManager 리팩토링 임시저장

--- Manager/SceneChangeManager.cs
+++ Manager/SceneChangeManager.cs
@@ -14,52 +14,64 @@ public class SceneChangeManager : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
-    private Vector3 SecondFloorPlayer = new Vector3(0.3f, 0.4f, 0.4f);
+    private Vector3 UnderFloorPlayer = new Vector3(0.3f, 0.4f, 0.4f);
 
 
     public void Awake()
     {
         //Managers 클래스의 SceneChangeManager 인스턴스 참조
         SceneChangeManager sceneChangeManager = Managers.SceneChange;
-
         //씬이 로드될 때 호출되는 이벤트 등록
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)//씬 전환 시 플레이어 스폰 처리
     {
-        if(scene.name== "New1_2FloorScene")//지상 씬에 스폰될 때
-        {
-            SceneCount++;
-            Debug.Log($"SceneCount : {SceneCount}");
+        SceneCount++;
+        Debug.Log("SceneCount : " + SceneCount);
+        GameObject SpawnPoint;
 
-            GameObject spawnPoint;//스폰포인트 오브젝트 찾기
-            if(SceneCount==1)//게임 첫 시작
-            {
-                spawnPoint = GameObject.Find("FirstSpawnPoint");
-            }
-            else//지하 -> 지상으로 돌아올 때
-            {
-                spawnPoint = GameObject.Find("LastSpawnPoint");
-            }
-            if (spawnPoint != null)//스폰포인트가 존재하면 플레이어 위치 이동
-            {
-                player.transform.position = spawnPoint.transform.position;
-                player.transform.rotation = spawnPoint.transform.rotation;
-                Debug.Log($"Spawn Point : {spawnPoint.name}");
-            }
-        }
-        else if(scene.name== "NewUnderAfter")//지상 -> 지하로 이동 시
+        if(scene.name=="New1_2FloorScene")
         {
-            GameObject spawnPoint = GameObject.Find("SecondSpawnPoint");
-            if(spawnPoint != null)
+            if(SceneCount==1)
             {
-                player.transform.position = spawnPoint.transform.position;
-                player.transform.rotation = spawnPoint.transform.rotation;
-                player.transform.localScale = SecondFloorPlayer;
-                Debug.Log("Last Spawn Point");
+                SpawnPoint = GameObject.Find("FirstSpawnPoint");
+            }
+            else
+            {
+                SpawnPoint = GameObject.Find("LastSpawnPoint");  
+            }
+            SPSetting(SpawnPoint);//스폰 시 플레이어의 로테이션 포지션을 스폰장소에 맞춘다.
+        }
+        if(scene.name=="NewUnder3F")
+        {
+            SpawnPoint  = GameObject.Find("SecondSpawnPoint");
+            SPSettingUnder(SpawnPoint);//지하는 플레이어 스케일 변경이 필요하므로 메서드를 따로 추가
+        }
+        if(scene.name =="NewUnderAfter")
+        {
+            SpawnPoint  = GameObject.Find("ThirdSpawnPoint");
+            SPSettingUnder(SpawnPoint);
+        }
+    }
+
+    public void SPSettingUnder(GameObject sp)
+    {
+          if(sp != null)
+            {
+                player.transform.position = sp.transform.position;
+                player.transform.rotation = sp.transform.rotation;
+                player.transform.localScale = UnderFloorPlayer;
+                Debug.Log($"Spawn Point : {sp.name}");
              }
-
-        }
+    }
+    public void SPSetting(GameObject sp)
+    {
+         if (sp != null)//스폰포인트가 존재하면 플레이어 위치 이동
+            {
+                player.transform.position = sp.transform.position;
+                player.transform.rotation = sp.transform.rotation;
+                Debug.Log($"Spawn Point : {sp.name}");
+            }
     }
 }
