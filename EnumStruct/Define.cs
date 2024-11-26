fceda5a481b784c246bf2a1ddb5139a5c7bdfd0b
SHIN BEOM SU <91828379+shin0624@users.noreply.github.com>
2024-11-26 18:53:03 +0900

    에너미 구조체

---
+++ EnumStruct/Define.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define 
+{
+    // 캐릭터 애니메이션 등 처리를 위한 열거체 클래스
+
+    public enum EnemyState
+    {
+        IDLE,
+        WALKING,
+        RUNNING,
+        ATTACK,
+    }
+
+    public enum DoctorState{
+        IDLE,
+        WALKING,
+        RUNNING,
+        ATTACK,
+    }
+}
