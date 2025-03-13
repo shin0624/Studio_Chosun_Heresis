f2d27337ffaf328c5a987c61de272b4a1f096dfc
SHIN BEOM SU <91828379+shin0624@users.noreply.github.com>
2025-03-13 16:12:32 +0900

    Update SequenceNode.cs

--- NewFiles/Enemy/Shaman/SequenceNode.cs
+++ NewFiles/Enemy/Shaman/SequenceNode.cs
@@ -18,10 +18,18 @@ public class SequenceNode : BehaviorNode
         
         switch(childStatus)
         {
-            case Status.Running : return Status.Running;//실행 중 --> 12.22 오류 발견 : 계속 Running을 반환하고 있어서 시퀀스 평가가 중단되어버림. --> idleActionNode를 변경
-            case Status.Success : current++; return current >=children.Count ? Status.Success : Status.Running;//각 노드가 성공이면 current 증가 후 success 반환, 다음 자식으로 이동
-            case Status.Failure : current=0; return Status.Failure;//실패
-            default : return Status.Success;// 디폴트 성공 반환
+            case Status.Running : 
+                return Status.Running;//실행 중 --> 12.22 오류 발견(해결) : 계속 Running을 반환하고 있어서 시퀀스 평가가 중단되어버림. --> idleActionNode 리턴값을 Success로 수정
+                
+            case Status.Success : 
+                current++; 
+                return current >=children.Count ? Status.Success : Status.Running;//각 노드가 성공이면 current 증가 후 success 반환, 다음 자식으로 이동
+                
+            case Status.Failure : 
+                current=0; 
+                return Status.Failure;//실패
+                
+            default : return Status.Success;// 플레이어가 감지되지 않았거나 특정 행동을 하지 않는 경우 Idle 상태 유지
         }
     }
 }
